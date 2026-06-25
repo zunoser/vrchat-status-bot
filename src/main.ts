fd68fb3f6a1b6c30a39a52f1308345ef8eb725e0
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-06-25 22:56:17 +0900

    feat: onlineとofflineでログ出すようにしてみた

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { vrcWS, type WebSocketContent, type HandlerType } from "./vrchat/websocket.ts";
 
 const logger: HandlerType<"friend-online"> = (content) => {
-  console.log(content);
+  if (!content.user){
+    return
+  }
+  console.log(`Online: ${content.user.displayName}`);
+};
+
+const loggers: HandlerType<"friend-offline"> = (content) => {
+  console.log(`Offline: ${content.userId}`);
 };
 const vrcws = vrcWS();
 vrcws.addHandler("friend-online", logger);
+vrcws.addHandler("friend-offline", loggers);
 
